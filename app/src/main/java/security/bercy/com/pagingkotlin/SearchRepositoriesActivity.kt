8e7a450910e0f0301945eeffabe93b3f7aeda35b
Bercy <adnmks@gmail.com>
2019-03-10 14:41:21 -0400

    Initial commit

---
+++ app/src/main/java/security/bercy/com/pagingkotlin/SearchRepositoriesActivity.kt
@@ -0,0 +1,14 @@
+package security.bercy.com.pagingkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchRepositoriesActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_repositories)
+    }
+}
